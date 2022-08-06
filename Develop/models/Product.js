f6a8ee7bf67d7a28c345956452788ee85a863d4b
Tyler Zeman <tylerzeman1999@gmai9l.com>
2022-08-05 23:26:06 -0500

    defined columns

--- Develop/models/Product.js
+++ Develop/models/Product.js
@@ -10,6 +10,26 @@ class Product extends Model {}
 Product.init(
   {
     // define columns
+    id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: true
+    },
+    product_id: {
+      type: DataTypes.INTEGER,
+      references: {
+        model: 'product',
+        key: 'id'
+      }
+    },
+    tag_id: {
+      type: DataTypes.INTEGER,
+      references: {
+        model: 'tag',
+        key: 'id'
+      }
+    }
   },
   {
     sequelize,

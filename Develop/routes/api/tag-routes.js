707116bc523ce87a4706dc2c3416b6a5422cbbfd
Tyler Zeman <tylerzeman1999@gmai9l.com>
2022-08-15 19:23:23 -0500

    added tag routes

--- Develop/routes/api/tag-routes.js
+++ Develop/routes/api/tag-routes.js
@@ -6,23 +6,47 @@ const { Tag, Product, ProductTag } = require('../../models');
 router.get('/', (req, res) => {
   // find all tags
   // be sure to include its associated Product data
+  Tag.findAll({
+    include: Product
+  })
+  .then(tagData => res.json(tagData))
 });
 
 router.get('/:id', (req, res) => {
   // find a single tag by its `id`
   // be sure to include its associated Product data
+  tag.findOne({
+    where: {
+      id: req.params.id
+    }, include: Product
+  }).then(tagInfo => res.json(tagInfo))
 });
 
 router.post('/', (req, res) => {
   // create a new tag
+  Tag.create({
+    tagName: req.body.tagName
+  }, {
+    where: {id:req.params.id}
+  }),then(tagData => res.json(tagData))
 });
 
 router.put('/:id', (req, res) => {
   // update a tag's name by its `id` value
+  Tag.update({
+    tagName: req.body.tagName
+  }, {
+    where: {id: req.params.id}
+  }).then(tagData => res.json(tagData))
 });
 
 router.delete('/:id', (req, res) => {
   // delete on tag by its `id` value
+  Tag.destroy({
+    where: {
+      id: req.params.id
+    }
+  }).then(tagData => res.json(tagData))
 });
 
 module.exports = router;
